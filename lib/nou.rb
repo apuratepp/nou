f564d6f360fe2e17566baf8390af582056409041
Josep <apuratepp@gmail.com>
2013-06-09 23:03:04 +0200

    scaffold && IO.popen tests

---
+++ lib/nou.rb
@@ -0,0 +1,34 @@
+module Nou
+	module Commands
+		class Logs
+			def initialize
+				if system('source ~/.nou')
+
+				else
+					puts "Please, create a ~/.nou file"
+				end
+			end
+
+			def call(args, options)
+				p ENV['SERVER']
+				# system 'ssh linode.equip9.org ls -l'
+			end	
+		end
+
+		class Meteo
+			def call(args, options)
+				system 'open http://meteoub.equip9.org/'
+			end
+		end
+
+		class While
+			def call(args, options)
+				IO.popen('while(true); do du -hs *; sleep 1; clear; done') do |io|
+    			io.each do |line|
+      			puts line
+    			end
+  			end
+			end
+		end
+	end
+end
